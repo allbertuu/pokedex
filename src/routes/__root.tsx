9f6c8ec569d0f05f507b93848e601558d95536f6
Alberto <albertovinicius3@gmail.com>
2026-02-12 20:05:26 -0300

    feat(home): add search and filtering functionality for `PokémonsList` ML3

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,14 @@
 import { TanStackDevtools } from "@tanstack/react-devtools";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+import { PokemonsListProvider } from "@/contexts/PokemonsList";
 
 export const Route = createRootRoute({
 	component: () => (
 		<>
-			<Outlet />
+			<PokemonsListProvider>
+				<Outlet />
+			</PokemonsListProvider>
 
 			<TanStackDevtools
 				config={{
